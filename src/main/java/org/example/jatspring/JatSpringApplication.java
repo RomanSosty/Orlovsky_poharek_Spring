9ba2cca9ded67cebfd5330ac0159107a85c408e6
Roman Šostok <102825558+RomanSosty@users.noreply.github.com>
2024-01-16 20:04:47 +0100

    Settings for project

---
+++ src/main/java/org/example/jatspring/JatSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.jatspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JatSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JatSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jatspring/JatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jatspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JatSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
