0a14106169933b343a01e3d95d99ac7fd8957972
Roman Šostok <102825558+RomanSosty@users.noreply.github.com>
2024-01-27 11:32:18 +0100

    add loginPage.html

--- src/main/resources/static/assets/function/validateForm.js
+++ src/main/resources/static/assets/function/validateForm.js
@@ -9,15 +9,7 @@ form.addEventListener("submit", function (event) {
 });
 
 document.getElementById("login").addEventListener("click", function () {
-  var url = new XMLHttpRequest();
-  url.onreadystatechange = function () {
-    if (url.readyState === 4 && url.status === 200) {
-      window.location.href = "login.jsp";
-    }
-  };
-
-  url.open("GET", "loginPage", true);
-  url.send();
+      window.location.href = "loginPage.html";
 });
 
 function validateForm() {
