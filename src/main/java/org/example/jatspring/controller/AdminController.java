cc692f991938ee8eba9b61332342448af1b020ce
sostok <sostok@oksystem.cz>
2024-01-30 10:14:24 +0100

    add new summary for Admin

--- src/main/java/org/example/jatspring/controller/AdminController.java
+++ src/main/java/org/example/jatspring/controller/AdminController.java
@@ -1,13 +1,28 @@
 package org.example.jatspring.controller;
 
+import org.example.jatspring.entity.ApplicationForm;
+import org.example.jatspring.service.ApplicationService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 @Controller
 public class AdminController {
+
+    private final ApplicationService applicationService;
+
+    public AdminController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
     @RequestMapping("/adminPage")
     @PreAuthorize("hasRole('ADMIN')")
-    public String getAdminPage() {
+    public String getAdminPage(Model model) {
+        List<ApplicationForm> applicationForms =applicationService.getAllForms();
+        model.addAttribute("forms", applicationForms);
         return "adminPage";
     }
 }
